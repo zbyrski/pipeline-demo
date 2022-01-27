4f6c10c0540e3ad8959ce67d6312110bb44eba3d
gattietime <gattietime@gmail.com>
2022-01-27 15:02:44 -0500

    switched to port 7000

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -15,6 +15,6 @@ public class App {
             ctx.status(200);
         })); //ctx represents the context of the request/response for the Handler
 
-        app.start();
+        app.start(7000);
     }
 }
